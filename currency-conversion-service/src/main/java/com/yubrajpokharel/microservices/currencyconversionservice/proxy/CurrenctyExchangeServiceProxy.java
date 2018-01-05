050f849608d7c77b32d2322c29027dc49149d203
yubrajpokharel <yubrajpokharel>
2018-01-05 01:56:12 -0600

    configuring eureka

--- currency-conversion-service/src/main/java/com/yubrajpokharel/microservices/currencyconversionservice/proxy/CurrenctyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/yubrajpokharel/microservices/currencyconversionservice/proxy/CurrenctyExchangeServiceProxy.java
@@ -2,6 +2,7 @@ package com.yubrajpokharel.microservices.currencyconversionservice.proxy;
 
 import com.yubrajpokharel.microservices.currencyconversionservice.model.CurrencyConversionBean;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
  * Created by yubrajpokharel on 1/5/18.
  */
 
-@FeignClient(name = "currency-exchange-server", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-server", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-server")
+@RibbonClient(name = "currency-exchange-server")
 public interface CurrenctyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
